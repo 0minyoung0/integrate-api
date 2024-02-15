89776732358db521a28892f6ff9ec95ce57184e3
정민영/연구원/ID기업/교육솔루션Project(minyoung.jeong@lge.com) <minyoung.jeong@lge.com>
2024-02-15 18:11:50 +0900

    5. Context와 함께 사용하기

---
+++ src/UsersContext.js
@@ -0,0 +1,64 @@
+import { createContext, useReducer, useContext } from "react";
+import {
+  createAsyncDispatcher,
+  createAsyncHandler,
+  initialAsyncState,
+} from "./asyncActionUtils";
+import * as api from "./api";
+
+const initialState = {
+  users: initialAsyncState,
+  user: initialAsyncState,
+};
+
+const usersHandler = createAsyncHandler("GET_USERS", "users");
+const userHandler = createAsyncHandler("GET_USER", "user");
+
+function usersReducer(state, action) {
+  switch (action.type) {
+    case "GET_USERS":
+    case "GET_USERS_SUCCESS":
+    case "GET_USERS_ERROR":
+      return usersHandler(state, action);
+    case "GET_USER":
+    case "GET_USER_SUCCESS":
+    case "GET_USER_ERROR":
+      return userHandler(state, action);
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+const UserStateContext = createContext(null);
+const UserDispatchContext = createContext(null);
+
+export function UsersProvider({ children }) {
+  const [state, dispatch] = useReducer(usersReducer, initialState);
+  return (
+    <UserStateContext.Provider value={state}>
+      <UserDispatchContext.Provider value={dispatch}>
+        {children}
+      </UserDispatchContext.Provider>
+    </UserStateContext.Provider>
+  );
+}
+
+export function useUsersState() {
+  const state = useContext(UserStateContext);
+  if (!state) {
+    throw new Error("Cannot find UsersProvider");
+  }
+  return state;
+}
+
+export function useUsersDispatch() {
+  const dispatch = useContext(UserDispatchContext);
+  if (!dispatch) {
+    throw new Error("Cannot find UsersProvider");
+  }
+  return dispatch;
+}
+
+export const getUsers = createAsyncDispatcher("GET_USERS", api.getUsers);
+
+export const getUser = createAsyncDispatcher("GET_USER", api.getUser);
